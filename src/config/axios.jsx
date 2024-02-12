0273efcc51d50175e0d9f445c9337678373b8b5d
master-dhanesh <dhanesh1296@gmail.com>
2024-02-12 14:20:11 +0530

    link added

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL:
+        "https://restful-api-w35e.onrender.com" || "http://localhost:8080/",
     withCredentials: true,
 });
 
